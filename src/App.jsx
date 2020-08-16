632326668e0b7fcebf794c3aaadcf216312ccd90
Artsem Virshych <Artsem_Virshych@epam.com>
2020-08-16 16:45:21 +0300

    delete all

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './App.sass';
-
-const App = () => <div className="test1">App</div>;
-
-export default App;
--- tests/example.test.js
+++
@@ -1,8 +0,0 @@
-describe('example.test.js', () => {
-    it('test: it should done', () => {
-        const testFirst = 'test';
-        const testSecond = 'test';
-
-        expect(testFirst).toEqual(testSecond);
-    });
-});
